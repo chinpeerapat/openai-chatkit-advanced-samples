a98435dd06fa530700e7986a57bc4ef456b7bbc6
chinpeerapat <88846925+chinpeerapat@users.noreply.github.com>
2025-10-11 00:38:28 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,6 +24,8 @@ export default defineConfig({
       // You can remove these examples added just to demonstrate how to configure the allowlist
       ".ngrok.io",
       ".trycloudflare.com",
+      ".chatkit-frontend.vercel.app",
+      ".chatkit-backend.vercel.app"
     ],
   },
 });
